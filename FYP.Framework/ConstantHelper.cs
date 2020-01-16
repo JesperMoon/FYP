01c0bde49d42b7ef1c43e08099f8f3bb43557220
JesperMoon <chewcarlyong@gmail.com>
2020-01-16 17:34:33 +0800

    Added Practitioner and Patient Login page

---
+++ FYP.Framework/ConstantHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYP.Framework
+{
+    public static partial class ConstantHelper
+    {
+        public static class AppSettings
+        {
+            public static string RootSiteUrl = ConfigurationManager.AppSettings["RootSiteUrl"];
+        }
+
+        public static class DBAppSettings
+        {
+            //public static string DbConnectionString = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+        }
+
+        public static class API
+        {
+            public const string PractitionerLogin = "/HomePage/PractitionerLogin/";
+            public const string PatientLogin = "/HomePage/PatientLogin/";
+        }
+
+    }
+}
--- FYP.Web/Controllers/HomePageController.cs
+++ FYP.Web/Controllers/HomePageController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FYP.Entities;
+using FYP.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,18 @@ namespace FYP.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult PractitionerLogin()
+        {
+            LoginInfo loginInfo = new LoginInfo();
+            return View(loginInfo);
+        }
+
+        public ActionResult PatientLogin()
+        {
+            LoginInfo loginInfo = new LoginInfo();
+            return View(loginInfo);
         }
     }
 }
